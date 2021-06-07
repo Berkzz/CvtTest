79656f6e5b5986a219b44bf817221972b77c8ec1
Maxim Popov <crissgmod@gmail.com>
2021-06-07 16:23:42 +0400

    Added UserContext Added Id to User

--- Models/User.cs
+++ Models/User.cs
@@ -7,6 +7,7 @@ namespace CvtTest.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Patronymic { get; set; }
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace CvtTest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +25,11 @@ namespace CvtTest
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStaticFiles();
+          //      app.UseMvcWithDefaultRoute();
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
