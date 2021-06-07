63fed8fc22be15da1215c2b3f08f9c3bc7c4f42f
Maxim Popov <crissgmod@gmail.com>
2021-06-07 16:02:01 +0400

    Added user model

---
+++ Models/ContactInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CvtTest.Models
+{
+    public class ContactInformation
+    {
+        public List<string> PhoneNumber { get; set; }
+        public List<string> Email { get; set; }
+        public List<string> Skype { get; set; }
+        public string Other { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CvtTest.Models
+{
+    public class User
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string DayOfBirth { get; set; }
+        public string Organization { get; set; }
+        public string Position { get; set; }
+        public ContactInformation Contacts { get; set; }
+    }
+}
